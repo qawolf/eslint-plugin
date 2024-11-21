2bcf7efbaf8078dfce4e24a5dd9a570103305962
Denis Sokolov <denis@sokolov.cc>
2024-11-21 12:00:00 +0000

    config: remove node import rules that require tailored tweaking

--- src/config/imports.ts
+++ src/config/imports.ts
@@ -4,15 +4,4 @@ export const importVerificationRules = {
   "@qawolf/restrict-react-namespace": "error",
 
   "import/no-duplicates": "error",
-
-  "node/no-extraneous-import": "error",
-  "node/no-extraneous-require": "error",
-  "node/no-unpublished-import": [
-    "error",
-    { tryExtensions: [".mjs", ".js", ".json", ".ts", ".tsx", ".d.ts"] },
-  ],
-  "node/no-unpublished-require": [
-    "error",
-    { tryExtensions: [".js", ".json", ".ts", ".tsx", ".d.ts"] },
-  ],
 } satisfies Linter.Config["rules"];
