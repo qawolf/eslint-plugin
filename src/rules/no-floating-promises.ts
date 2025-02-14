4b2a710432b0c72888609efe21186a19e575339f
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2025-02-14 11:25:13 -0800

    don't mutate

--- src/rules/no-floating-promises.ts
+++ src/rules/no-floating-promises.ts
@@ -4,7 +4,15 @@ const baseRule = tsPlugin.rules["no-floating-promises"];
 
 if (!baseRule) throw Error("no-floating-promises is not available");
 
-baseRule.meta.messages.floatingVoid +=
-  " If you’re using `useEffect`, try some helper function like `useAsyncEffectAndCatch`.";
-
-export default baseRule!;
+export default {
+  ...baseRule,
+  meta: {
+    ...baseRule.meta,
+    messages: {
+      ...baseRule.meta.messages,
+      floatingVoid:
+        baseRule.meta.messages.floatingVoid +
+        " If you're using `useEffect`, try some helper function like `useAsyncEffectAndCatch`.",
+    },
+  },
+};
