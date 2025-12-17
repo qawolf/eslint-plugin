c49c6ab541520d47b592b4db7620b4077c242cc8
Denis Sokolov <denis@sokolov.cc>
2025-12-17 12:00:00 +0000

    imports: fix findBoundary not being able to compare same modules

--- src/imports/modules/index.ts
+++ src/imports/modules/index.ts
@@ -3,7 +3,7 @@ export function findBoundary(module1: string, module2: string): string {
   const parts2 = module2.split("/");
 
   let i = 0;
-  while (parts1[i] === parts2[i]) i++;
+  while (parts1[i] === parts2[i] && i < parts1.length) i++;
 
   return parts1.slice(0, i).join("/");
 }
